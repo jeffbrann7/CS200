5dae159d318318334a3b4282a960cb228500f3a9
jeffbrann7 <jbrann6@stumail.jccc.edu>
2017-01-19 18:58:13 -0600

    Add files via upload

---
+++ Labs/example.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Variable declarations
+    string name, color;
+	string name;
+	string color;
+	string adjective;
+
+    // Output and input
+    cout << "Hello, what is your name? ";
+    cin >> name;
+
+    cout << "What is your favorite color? ";
+    cin >> color;
+
+    cout << endl;
+    cout << "So can I call you " << color << " " << name << "?" << endl;
+
+	// test 123
+
+    return 0;
+}
