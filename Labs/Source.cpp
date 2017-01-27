c613b1debd633fd6e447028101300d9fe8db105a
jeffbrann7 <jbrann6@stumail.jccc.edu>
2017-01-26 19:02:55 -0600

    Add files via upload

--- Labs/Source.cpp
+++ Labs/Source.cpp
@@ -1,55 +1,27 @@
 #include <iostream>
-#include <string> 
+#include <string>
 using namespace std;
 
 int main()
 {
-	float a;
-	float b;
-	int choice;
+	string homeTown;
 
-	cout << "Enter a value for a: ";
-	cin >> a;
-	cout << endl;
+	cout << "What is your home town? ";
+	cin >> homeTown;
 
-	cout << "Enter a value for b: ";
-	cin >> b;
-	cout << endl;
-	cout << endl;
-	
-	cout << "1. Add" << endl;
-	cout << "2. Subtract" << endl;
-	cout << "3. Multiply" << endl;
-	cout << "4. Divide" << endl;
-	cout << "Select an operation to use: ";
-	cin >> choice;
-	cout << endl;
-	
-	switch (choice)
+	if (homeTown.size() > 6)
 	{
-		case 1:
-			cout << a + b << endl;
-			break;
-
-		case 2: 
-			cout << a - b << endl;
-			break;
-
-		case 3:
-			cout << a * b << endl;
-			break;
-
-		case 4:
-			cout << a / b << endl;
-			break;
-
-		default: 
-			cout << "Invalid Choice" << endl;
+		cout << "That's a long name!" << endl;
 	}
-	
 
-
+	string name;
 	
-	while (true) {};
+	cout << "What is your name? ";
+	cin >> name;
+	
+	cout << "Hi there " << name 
+		<< " from " << homeTown << endl;
+
+	while (true) {}
 	return 0;
 }
