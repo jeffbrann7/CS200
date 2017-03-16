65089244c8f66e2683b53f4f0dc56dc70d199996
jeffbrann7 <jbrann6@stumail.jccc.edu>
2017-03-16 16:10:46 -0500

    Add files via upload

---
+++ Labs/Pointerslabex1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number = 20;
+	float wage = 9.99;
+	string name = "Bob";
+
+	int* ptrInteger;
+	float* ptrFloat;
+	string* ptrString;
+
+	cout << &number << " = " << number << endl;
+	cout << &wage << " = " << wage << endl;
+	cout << &name << " = " << name << endl;
+
+
+
+
+
+
+	return 0;
+}
